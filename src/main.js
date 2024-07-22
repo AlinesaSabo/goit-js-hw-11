89911fa6e60404437307054d5de937ac13d63c7e
Alinesa <alinesa322@gmail.com>
2024-07-22 14:59:15 +0300

    add

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-<div class="gallery">
-  <a href="images/image1.jpg">
-    <img src="images/thumbs/thumb1.jpg" alt="" title="" />
-  </a>
-  <a href="images/image2.jpg">
-    <img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image" />
-  </a>
-</div>;
+// <div class="gallery">
+//   <a href="images/image1.jpg">
+//     <img src="images/thumbs/thumb1.jpg" alt="" title="" />
+//   </a>
+//   <a href="images/image2.jpg">
+//     <img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image" />
+//   </a>
+// </div>;
 
-[refresh()](<https://github.com/andreknieriem/simplelightbox#public-methods>)
+// [refresh()](<https://github.com/andreknieriem/simplelightbox#public-methods>)

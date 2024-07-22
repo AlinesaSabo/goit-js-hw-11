b7eac7473d7957102aebab0763f21f375939d4d4
Alinesa <alinesa322@gmail.com>
2024-07-22 14:57:28 +0300

    add

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+<div class="gallery">
+  <a href="images/image1.jpg">
+    <img src="images/thumbs/thumb1.jpg" alt="" title="" />
+  </a>
+  <a href="images/image2.jpg">
+    <img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image" />
+  </a>
+</div>;
+
+[refresh()](<https://github.com/andreknieriem/simplelightbox#public-methods>)

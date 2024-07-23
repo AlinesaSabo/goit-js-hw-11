4faf59bfa3477ea9ae02cb640a964a3c95bfb0f7
Alinesa <alinesa322@gmail.com>
2024-07-24 01:55:41 +0300

    Aadd

--- src/main.js
+++ src/main.js
@@ -2,14 +2,56 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { searchImagesByQuery } from './js/pixabay-api';
+import { renderImageSearch } from './js/render-functions';
 
-// <div class="gallery">
-//   <a href="images/image1.jpg">
-//     <img src="images/thumbs/thumb1.jpg" alt="" title="" />
-//   </a>
-//   <a href="images/image2.jpg">
-//     <img src="images/thumbs/thumb2.jpg" alt="" title="Beautiful Image" />
-//   </a>
-// </div>;
+const gallery = document.querySelector('.gallery');
+const searchForm = document.querySelector('.search-form');
+const loadingSpinner = document.querySelector('.loader');
 
-// [refresh()](<https://github.com/andreknieriem/simplelightbox#public-methods>)
+searchForm.addEventListener('submit', handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const queryValue = form.elements.query.value.trim().toLowerCase();
+  showLoadingSpinner();
+
+  searchImagesByQuery(queryValue)
+    .then(data => {
+      gallery.innerHTML = '';
+
+      if (data.hits.length === 0) {
+        iziToast.error({
+          titleColor: '#FFFFFF',
+          backgroundColor: '#EF4040',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#FFFFFF',
+          position: 'topRight',
+          iconColor: '#FFFFFF',
+        });
+        return;
+      }
+
+      renderImageSearch(data);
+      new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+    })
+    .catch()
+    .finally(() => {
+      hideLoadingSpinner();
+      form.reset();
+    });
+}
+
+function showLoadingSpinner() {
+  loadingSpinner.style.display = 'block';
+}
+
+function hideLoadingSpinner() {
+  loadingSpinner.style.display = 'none';
+}
